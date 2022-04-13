909e2fe927b19e2679fc816564848992e878abe6
Oluwagbade Oluwamayowa <31127611+mykhaelmayor@users.noreply.github.com>
2022-04-13 08:00:28 +0100

    updated the mainactivity code.still works the same way

--- app/src/main/java/com/example/bioprofileapp/MainActivity.kt
+++ app/src/main/java/com/example/bioprofileapp/MainActivity.kt
@@ -1,26 +1,27 @@
 package com.example.bioprofileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import kotlin.random.Random
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    private val attributeList = arrayListOf("hardworking", "lazy", "somewhat lazy", "very lazy", "dedicated")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val editTxt: TextView= findViewById(R.id.randomstory)
-        val randomBtn: Button= findViewById(R.id.rdmbutton)
+        val randomBtn: Button = findViewById(R.id.rdmbutton)
+        randomBtn.setOnClickListener { randomizer() }
+    }
 
-        randomBtn.setOnClickListener {
-           val random = Random
-            val randomText = random.nextInt(attributeList.count())
-            editTxt.text = attributeList[randomText]
+    private fun randomizer() {
+        val attributeList =
+            arrayListOf("hardworking", "lazy", "somewhat lazy", "very lazy", "dedicated")
+        val randomText = attributeList.random()
 
-        }
+        val editTxt: TextView = findViewById(R.id.randomstory)
+        editTxt.text = randomText
     }
 }
